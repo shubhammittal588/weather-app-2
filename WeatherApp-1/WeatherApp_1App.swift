b514547e9432fe2489f932195d9242a577b98b0f
Shubham Mittal <shubhammittal588@gmail.com>
2023-03-20 21:05:20 +0530

    Initial Commit

---
+++ WeatherApp-1/WeatherApp_1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_1App.swift
+//  WeatherApp-1
+//
+//  Created by Shubham Mittal on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
